f94f1711c990b009bd4d64e8bbae19c40a66a4a2
aliatayee <aliaqaatayee@gmail.com>
2022-06-17 20:57:27 +0430

    Update api key

--- src/modules/scoreBoard.js
+++ src/modules/scoreBoard.js
@@ -3,7 +3,7 @@ import displayScore from './render.js';
 class Score {
   constructor() {
     this.scoreTable = document.querySelector('#score-table');
-    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SKyj4JbVvkGO9yiHwdV0/scores/';
+    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/3qWxqEPmfgDZUgyvSL9w/scores/';
   }
 
   render = () => {
